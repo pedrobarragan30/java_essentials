e057e1a650d7ded3733fd70293868630259b4254
PedroB <pedrobarragan30@gmail.com>
2018-02-03 13:37:53 -0600

    Corregir errores de compilacion

--- src/src4/FirstSelenium.java
+++ src/src4/FirstSelenium.java
@@ -1,10 +1,18 @@
+package src4;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class FirstSelenium {
 
     public static void main(String[] args) {
-        WebDriver driver = new FirefoxDriver();
-        String expectedTitle = "Welcome: Mercury Tours"
+        //System.setProperty("webdriver.gecko.driver", "C:\\test_automation\\drivers\\geckodriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\test_automation\\drivers\\chromedriver.exe");
+    	WebDriver driver = new ChromeDriver();
+        String expectedTitle = "Welcome: Mercury Tours";
         driver.get("http://newtours.demoaut.com");
-        actualTitle = driver.getTitle();
+        String actualTitle = driver.getTitle();
         if (actualTitle.contentEquals(expectedTitle)){
             System.out.println("Test Passed!");
         } else {
